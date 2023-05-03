bdf8309866706000055f574d9870716b929d6e0e
ZRoman87 <zrazhevskiy.roman@yandex.ru>
2023-05-03 19:40:35 +0300

    First Commit

---
+++ src/main/java/com/example/skyprospringhomework211/SkyproSpringHomework211Application.java
@@ -0,0 +1,13 @@
+package com.example.skyprospringhomework211;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkyproSpringHomework211Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SkyproSpringHomework211Application.class, args);
+    }
+
+}
